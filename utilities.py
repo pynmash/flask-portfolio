39341d239fd4bb0fd1f3a67075b30a2635bdb666
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-08 22:16:23 +0000

    Add projects area

---
+++ utilities.py
@@ -0,0 +1,10 @@
+cards = [{
+    'img': 'morse.jpg',
+    'text': 'Convert to and from morse code',
+    'gh-link': 'https://github.com/pynmash/inspector-morse'
+},
+{
+    'img': 'web.jpg',
+    'text': 'The website you\'re looking at now!', 
+    'gh-link': 'https://github.com/pynmash/flask-portfolio'
+}]
