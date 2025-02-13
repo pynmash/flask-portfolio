d66288b602c619596941e87321b5dec37613b501
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-13 23:59:43 +0000

    Add tic-tac-toe prject to cards

--- utilities.py
+++ utilities.py
@@ -1,10 +1,15 @@
 cards = [{
     'img': 'morse.jpg',
     'text': 'Convert to and from morse code',
-    'gh-link': 'https://github.com/pynmash/inspector-morse'
+    'gh_link': 'https://github.com/pynmash/inspector-morse'
 },
 {
     'img': 'web.jpg',
     'text': 'The website you\'re looking at now!', 
-    'gh-link': 'https://github.com/pynmash/flask-portfolio'
+    'gh_link': 'https://github.com/pynmash/flask-portfolio'
+},
+{
+    'img': 'tic-tac-toe.jpg',
+    'text': 'The classic game of X and O',
+    'gh_link':  'https://github.com/pynmash/tic-tac-toe'
 }]
